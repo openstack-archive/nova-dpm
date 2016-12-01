5bf432bfefd2ebf489b66343fed205c9d836ebd2
preethipy <preethi.py@in.ibm.com>
2016-12-01 16:05:21 +0530

    Driver code for CPCSubset listing feature
    - Driver code has been added which can list the CpcSubsets- subset of zSystems
    Central processor complex (CPC)
    - Dpm configuration files based on oslo config libraries that will read dpm
    specific attributes from nova.conf
    - All relevant Driver functions for listing CpcSubsets is implemented
    - Defaulted CPCSubset type to BAREMETAL which will be updated with HVType
    is updated with PRSM hypervisor type
    - Unit testcases for some of the files [WIP]
    
    Change-Id: Id447928fee253aac87f8a1da251f99a3386f53dd
    Partial-Bug: #1645999

---
+++ nova_dpm/tests/unit/virt/dpm/fakecpcs.py
@@ -0,0 +1,46 @@
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from zhmcclient._cpc import Cpc
+
+
+def getFakeCPC(cpcmanager):
+    cpc_props = dict()
+
+    cpc_props['object-uri'] = "/api/cpc/dummy"
+    cpc_props['name'] = "fakecpc"
+    cpc_props['storage-customer'] = 100
+    cpc_props['processor-count-pending-ifl'] = 6
+    cpc_props['processor-count-ifl'] = 12
+    cpc_props['storage-customer-available'] = 50
+
+    cpc = Cpc(cpcmanager, cpc_props['object-uri'], cpc_props)
+    return cpc
+
+
+def getFakeCPCconf():
+
+    conf = {'hostname': "S12subset",
+            'cpcname': "P0000S12",
+            'uuid': "1232132",
+            'max_processors': 10,
+            'max_memory_mb': 200,
+            'max_partitions': 10
+            }
+    return conf
+
+
+def getFakeCPCwithProp(cpcmanager, cpc_props):
+
+    cpc = Cpc(cpcmanager, cpc_props['object-uri'], cpc_props)
+    return cpc
