9ff9a9cfff0e8d2512c65f9e8b18c7af21766c3a
preethipy <preethi.py@in.ibm.com>
2016-12-01 16:05:21 +0530

    Driver code for CPCSubset listing feature
    - Driver code has been added which can list the CpcSubsets- subset of zSystems
    Central processor complex (CPC)
    - Dpm configuration files based on oslo config libraries that will read dpm
    specific attributes from nova.conf
    - All relevant Driver functions for listing CpcSubsets is implemented
    - Unit testcases for some of the files [WIP]
    
    Change-Id: Id447928fee253aac87f8a1da251f99a3386f53dd
    Partial-Bug: #1645999

---
+++ nova_dpm/compute/hv_type.py
@@ -0,0 +1,49 @@
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Constants and helper APIs for dealing with virtualization types
+
+The constants provide the standard names for all known guest
+virtualization types. This is not to be confused with the Nova
+hypervisor driver types, since one driver may support multiple
+virtualization types and one virtualization type (eg 'xen') may
+be supported by multiple drivers ('XenAPI' or  'Libvirt-Xen').
+"""
+
+from nova.objects import fields as obj_fields
+
+PRSM = ("prsm",)
+
+ALL = obj_fields.HVType.ALL + PRSM
+
+
+def is_valid(name):
+    """Check if a string is a valid hypervisor type
+
+    :param name: hypervisor type name to validate
+
+    :returns: True if @name is valid
+    """
+    return obj_fields.HVType.is_valid(name)
+
+
+def canonicalize(name):
+    """Canonicalize the hypervisor type name
+
+    :param name: hypervisor type name to canonicalize
+
+    :returns: a canonical hypervisor type name
+    """
+
+    return obj_fields.HVType.canonicalize(name)
