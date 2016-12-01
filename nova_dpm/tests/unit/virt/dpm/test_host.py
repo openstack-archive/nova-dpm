65ec36e75bfb8a0fada4e1b0ce4bf11e8bb64196
preethipy <preethi.py@in.ibm.com>
2016-12-01 16:05:21 +0530

    Driver code for CPCSubset listing feature
    - Driver code has been added which can list the CpcSubsets- subset of zSystems
    Central processor complex (CPC)
    - Dpm configuration files based on oslo config libraries that will read dpm
    specific attributes from nova.conf
    - All relevant Driver functions for listing CpcSubsets is implemented
    - Defaulted CPCSubset type to BAREMETAL which will be updated with HVType
    is updated with PRSM hypervisor type
    - Unit testcases for some of the files [WIP]
    
    Change-Id: Id447928fee253aac87f8a1da251f99a3386f53dd
    Partial-Bug: #1645999

---
+++ nova_dpm/tests/unit/virt/dpm/test_host.py
@@ -0,0 +1,68 @@
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import mock
+
+from nova.test import TestCase
+from nova_dpm.tests.unit.virt.dpm import fakecpcs
+from nova_dpm.tests.unit.virt.dpm import fakezhmcclient
+from nova_dpm.virt.dpm import host
+
+
+"""
+cpcsubset unit testcase
+"""
+
+
+class HostTestCase(TestCase):
+
+    def setUp(self):
+        super(HostTestCase, self).setUp()
+
+    @mock.patch.object(host.LOG, 'warning')
+    def test_host(self, mock_warning):
+
+        session = fakezhmcclient.Session("dummy", "dummy", "dummy")
+        client = fakezhmcclient.Client(session)
+
+        cpcmanager = fakezhmcclient.getCpcmgrForClient(client)
+        cpc = fakecpcs.getFakeCPC(cpcmanager)
+        conf = fakecpcs.getFakeCPCconf()
+
+        host.Host(conf, cpc, client)
+
+        expected_arg = {'hostname': 'fakecpc'}
+        assertlogs = False
+        for call in mock_warning.call_args_list:
+
+            if (len(call) > 0):
+                if (len(call[0]) > 1 and call[0][1] == expected_arg):
+                    assertlogs = True
+
+        self.assertTrue(assertlogs)
+
+    @mock.patch.object(host.LOG, 'warning')
+    def test_host_properties(self, mock_warning):
+
+        session = fakezhmcclient.Session("dummy", "dummy", "dummy")
+        client = fakezhmcclient.Client(session)
+
+        cpcmanager = fakezhmcclient.getCpcmgrForClient(client)
+        cpc = fakecpcs.getFakeCPC(cpcmanager)
+        conf = fakecpcs.getFakeCPCconf()
+
+        host1 = host.Host(conf, cpc, client)
+        host_properties = host1.properties
+        self.assertEqual(host_properties['hypervisor_hostname'], 'S12subset',
+                         'Hostname is S12subset')
+        self.assertEqual(host_properties['dpm_name'], 'fakecpc', 'dpm_name')
