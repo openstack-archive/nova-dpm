e51536fb86172cebe7c17023d10f9f8e276d63a8
Prabhat Ranjan <pranjank@in.ibm.com>
2016-12-16 15:06:51 +0530

    Adding partitioin properties mapper
    +Added mapper of nova properties to dpm partition properties
    
    Adding object which will get VM information
    
    + Adding InstanceInfo class which will give information of VM status
    which is required in get_info method of virt/dpm/driver.py
    
    Change-Id: Ifc67cebfcb2694ec082955a7180c0793ada20f21
    Signed-off-by: Prabhat Ranjan <pranjank@in.ibm.com>

---
+++ nova_dpm/virt/dpm/vm.py
@@ -0,0 +1,58 @@
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from nova.compute import power_state
+
+class InstanceInfo(object):
+    """Instance Information
+
+    This object loads VM information like state, memory used etc
+
+    """
+
+    @property
+    def state(self):
+
+        # TODO
+        # As of now returning dummy value
+        return power_state.RUNNING
+
+    @property
+    def mem(self):
+
+        # TODO
+        # As of now returning dummy value
+        return 1024
+
+    @property
+    def max_mem(self):
+
+        # TODO
+        # As of now returning dummy value
+        return 2048
+
+    @property
+    def num_cpu(self):
+
+        # TODO
+        # As of now returning dummy value
+        return 3
+
+    @property
+    def cpu_time(self):
+
+        # TODO
+        # As of now returning dummy value
+        return 100
