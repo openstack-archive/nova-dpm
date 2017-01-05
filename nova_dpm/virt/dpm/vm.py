8bbb3c1df564954f21602b84053cf94241ce4443
preethipy <preethi.py@in.ibm.com>
2017-01-05 18:26:29 +0530

    Volume attach, detach and HBA configuration
    + Spawn flow to attach HBA for a given partition
    using the configuration of Storage adapter and port
    along with some validation on configuration specified
    + Setting boot options as the first HBA adapter
    created.(This will be modified once Cinder integrated)
    + Volume attach and detach functions implemented which
    will get the supported Volume driver
    DpmFibreChannelVolumeDriver and invoke connect/disconnect
    options for attaching volume
    + Added testcases to the above functions except
    get_volume_connector function which is still not used
    until cinder integration is complete
    
    Change-Id: I26ea7dad7fb59ee2e2d29c0c37f9c0e0d35a7aeb

--- nova_dpm/virt/dpm/vm.py
+++ nova_dpm/virt/dpm/vm.py
@@ -185,16 +185,35 @@ class Instance(object):
         resources['block_device_info'] = block_device_info
         return resources
 
-    def get_hba_properties(self):
+    def get_hba_uris(self):
         LOG.debug('Get Hba properties')
+        self.partition.pull_full_properties()
+        return self.partition.properties['hba-uris']
+
+    def get_hba_wwpns(self):
+        LOG.debug('Get Hba wwpns')
+        hba_wwpns = []
+        if self.partition is not None:
+            hba_manager = self.partition.hbas
+            hbas = hba_manager.list(full_properties=False)
+            for hba in hbas:
+                hba_wwpns.append(hba.properties['wwpn'])
+        return hba_wwpns
+
+    def set_boot_properties(self, wwpn, lun, booturi):
+        LOG.debug('set_boot_properties')
+        bootProperties = {'boot-device': 'storage-adapter',
+                          'boot-storage-device': booturi,
+                          'boot-world-wide-port-name': wwpn,
+                          'boot-logical-unit-number': lun}
+        self.partition.update_properties(properties=bootProperties)
 
     def launch(self, partition=None):
         LOG.debug('Partition launch triggered')
         self.instance.vm_state = vm_states.BUILDING
         self.instance.task_state = task_states.SPAWNING
         self.instance.save()
-        bootproperties = self.get_boot_properties()
-        self.partition.update_properties(properties=bootproperties)
+
         result = self.partition.start(True)
         # TODO(preethipy): The below method to be removed once the bug
         # on DPM is fixed to return correct status on API return
@@ -205,7 +224,8 @@ class Instance(object):
         if self.partition:
             result = self.partition.stop(True)
             # TODO(preethipy): The below method to be removed once the bug
-            # on DPM is fixed to return correct status on API return
+            # on DPM is fixed to return correct status on API
+            # return
             self._loop_status_update(result, 5, 'stopped')
             if (self.partition.properties['status'] == 'stopped'):
                 self.partition.delete()
@@ -259,16 +279,6 @@ class Instance(object):
                           "for status check")
                 time.sleep(2)
                 iterations -= 1
-
-    def get_boot_properties(self):
-        LOG.debug('Retrieving boot properties for partition')
-        # TODO(preethipy): update the boot-device to storage-adapter
-        # TODO(preethipy): update the boot-storage-device with valid
-        # HBA uri
-        # TODO(preethipy): update boot-logical-unit-number and
-        # boot-world-wide-port-name
-        bootProperties = {'boot-device': 'test-operating-system'}
-        return bootProperties
 
     def get_partition(self, cpc, instance):
         partition = None
@@ -392,4 +402,6 @@ class PhysicalAdapterModel(object):
         # If no port-element-id was defined, default to 0
         # result[1] can also be '' - handled by 'and result[1]'
         port = int(result[1] if len(result) == 2 and result[1] else 0)
+        LOG.debug('Adapter ID: %(adapterid)s and Port: %(port)s'
+                  % {"adapterid": str(adapter_id), "port": str(port)})
         return adapter_id, port
