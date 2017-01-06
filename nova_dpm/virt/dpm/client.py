465fad1ea31c252d046c44e0561903a8e862d851
preethipy <preethi.py@in.ibm.com>
2017-01-06 11:14:34 +0530

    [WIP ]Refactor zhmcclient references to client
    Change-Id: Ib7a80e89d6a4a48c21f536dd1f6937d23f26bcf9

---
+++ nova_dpm/virt/dpm/client.py
@@ -0,0 +1,48 @@
+# Copyright 2016 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import requests.packages.urllib3
+
+from oslo_log import log as logging
+from oslo_utils import importutils
+
+zhmcclient = None
+LOG = logging.getLogger(__name__)
+
+
+class Client(object):
+
+    def get_zhmcclient(self):
+        """Lazy initialization for zhmcclient
+
+        This function helps in lazy loading zhmclient. The zhmcclient can
+        otherwise be set to fakezhmcclient for unittest framework
+        """
+
+        LOG.debug("get_zhmcclient")
+        # TODO(preethipy): The below line will be removed once the warnings are
+        # supressed within zhmclient code
+        requests.packages.urllib3.disable_warnings()
+
+        global zhmcclient
+        if zhmcclient is None:
+            zhmcclient = importutils.import_module('zhmcclient')
+
+        return zhmcclient
+
+    def get_client_for_sesion(self, zhmc, userid, password):
+        LOG.debug("get_client_for_sesion")
+        zhmcclient = self.get_zhmcclient()
+        session = zhmcclient.Session(zhmc, userid, password)
+        return zhmcclient.Client(session)
