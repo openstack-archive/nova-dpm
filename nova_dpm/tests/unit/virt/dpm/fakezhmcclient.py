42e5f40079310923ff8898fa8507a51c054509b1
Prabhat Ranjan <pranjank@in.ibm.com>
2017-01-11 18:04:57 +0530

    Use uuid for partition name
    Use uuid of instance for partition "name" parameter.
    And also use nova.conf.CONF.host for partition "description" parameter.
    
    Closes-Bug: #1655614
    
    Change-Id: Ice164d983db72b155f43405ed893810bfe2d3ac0
    Signed-off-by: Prabhat Ranjan <pranjank@in.ibm.com>

--- nova_dpm/tests/unit/virt/dpm/fakezhmcclient.py
+++ nova_dpm/tests/unit/virt/dpm/fakezhmcclient.py
@@ -146,7 +146,13 @@ class CpcManager(BaseManager):
         # Parameters:
         #   client (:class:`~zhmcclient.Client`):
         #      Client object for the HMC to be used.
-        super(CpcManager, self).__init__(Cpc)
+        query_props = [
+            'name',
+        ]
+        super(CpcManager, self).__init__(Cpc, None,
+                                         uri_prop='object-uri',
+                                         name_prop='name',
+                                         query_props=query_props)
         self._session = client.session
 
     def list(self, full_properties=False):
@@ -186,7 +192,13 @@ class PartitionManager(BaseManager):
         # Parameters:
         #   cpc (:class:`~zhmcclient.Cpc`):
         #     CPC defining the scope for this manager.
-        super(PartitionManager, self).__init__(cpc)
+        query_props = [
+            'name',
+        ]
+        super(PartitionManager, self).__init__(cpc, None,
+                                               uri_prop='object-uri',
+                                               name_prop='name',
+                                               query_props=query_props)
 
     def list(self, full_properties=False):
         partition_list = []
@@ -215,7 +227,13 @@ class NicManager(BaseManager):
         # Parameters:
         #   partition (:class:`~zhmcclient.Partition`):
         #     Partition defining the scope for this manager.
-        super(NicManager, self).__init__(partition)
+        query_props = [
+            'name',
+        ]
+        super(NicManager, self).__init__(partition, None,
+                                         uri_prop='object-uri',
+                                         name_prop='name',
+                                         query_props=query_props)
 
     def list(self, full_properties=False):
         nic_list = []
@@ -247,7 +265,13 @@ class HbaManager(BaseManager):
         # Parameters:
         #   partition (:class:`~zhmcclient.Partition`):
         #     Partition defining the scope for this manager.
-        super(HbaManager, self).__init__(partition)
+        query_props = [
+            'name',
+        ]
+        super(HbaManager, self).__init__(partition, None,
+                                         uri_prop='object-uri',
+                                         name_prop='name',
+                                         query_props=query_props)
 
     def list(self, full_properties=False):
         hba_list = []
@@ -279,7 +303,13 @@ class AdapterManager(BaseManager):
         # Parameters:
         #   partition (:class:`~zhmcclient.Partition`):
         #     Partition defining the scope for this manager.
-        super(AdapterManager, self).__init__(partition)
+        query_props = [
+            'name',
+        ]
+        super(AdapterManager, self).__init__(partition, None,
+                                             uri_prop='object-uri',
+                                             name_prop='name',
+                                             query_props=query_props)
 
     def list(self, full_properties=False):
         adapter_list = []
