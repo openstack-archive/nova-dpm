ec17d692c3dd9b1215a4ac1286919ebfc4c58d09
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2017-01-30 16:22:17 +0100

    Add DPM Guest Image Tools
    The guest image tools must be installed inside an operating system
    to allow automated network configuraiton.
    
    * autoconfigure_networking
      A systemd service that starts before cloud-init. It groups qeth
      devices to network interfaces that can be consumed by Linux.
      Therefore all devices listed in /proc/cmdline get configured with
      the provided portno in layer2 mode. The format of the data in the
      cmdline must be
    
            <devno>,<portno>[,<mac>];
    
    * Setmac
      A udev rule and the corresponding script to change the MAC address
      of a network interface from within the guests operating system.
      This is done along the data provided via /proc/cmdline. The format
      of the data in the cmdline must be
    
            <devno>,<portno>,<mac>;
    
    Note: The boot-os-specific-parameters property of a partition is used
    to pass in data from Nova into the operation system. This data is
    appended to /proc/cmdline.
    
    Change-Id: Iea199b085aca34769128a8691e95e790a396ee80

---
+++ guest_image_tools/usr/bin/autoconfigure_networking.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+# Copyright 2017 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOG_PREFIX="$0"
+REGEX_DEV_NO="([0-9a-f]{4})"
+REGEX_MAC="[0-9A-Fa-f]{12}"
+
+# Regex to match
+# <devno>,<port>;
+# <devno>,<port>,<mac>;
+regex="$REGEX_DEV_NO,([0-1])(,$REGEX_MAC)?;"
+
+#CMDLINE="some stuff 0001,1,000000000011;0004,;0007,0; more stuff"
+CMDLINE=`cat /proc/cmdline`
+
+function log {
+   # $1 message to log
+   echo "$LOG_PREFIX: $1"
+}
+
+log "Start"
+
+# Bash does not support global matching, therefore
+# we remove the current match from the cmdline to
+# allow matching the next value
+while [[ $CMDLINE =~ $regex ]]; do
+  dev_no="${BASH_REMATCH[1]}"
+  dev_bus_id="0.0.$dev_no"
+  log "Configuring dev_bus_id $dev_bus_id."
+
+  # remove current match from variable, to allow next match in next iteration
+  CMDLINE=${CMDLINE#*"${dev_no}"}
+
+
+  # Check if device is already configured
+  path="/sys/bus/ccwgroup/devices/$dev_bus_id"
+  if [ -d "$path" ]; then
+     log "Interface for $dev_bus_id already configured. Skipping."
+     continue
+  fi
+
+  # Determine port
+  port="${BASH_REMATCH[2]}"
+  log "Port $port used for dev_bus_id $dev_bus_id."
+
+  cmd="znetconf -a $dev_bus_id -o portno=$port,layer2=1"
+  eval $cmd
+
+done
+log "Finished"
