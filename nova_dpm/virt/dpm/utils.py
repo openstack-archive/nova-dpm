a4f05c9c0b5b4bb3236e7c7f67f77e32f1596d33
Prabhat Ranjan <pranjank@in.ibm.com>
2017-02-21 12:36:11 +0530

    [pike]Optimize nova compute
    nova compute start takes long (~25 seconds).
    The reason for this is, that the cpc object is retrieved 2 times.
    Optimized with one call.
    
    closes-Bug: 1655037
    
    Change-Id: I4946525f55414cb6dae7ef774f404d8cba30fedd
    Signed-off-by: Prabhat Ranjan <pranjank@in.ibm.com>

--- nova_dpm/virt/dpm/utils.py
+++ nova_dpm/virt/dpm/utils.py
@@ -22,35 +22,33 @@ LOG = logging.getLogger(__name__)
 def valide_host_conf(conf, cpc):
     LOG.debug('valide_host_conf')
     if (cpc.dpm_enabled):
-        cpc.pull_full_properties()
+        if (conf['max_processors'] > cpc.get_property('processor-count-ifl')):
+            # TODO(preethipy): Exception infrastructure to be finalized
+            errormsg = (_("max_processors %(config_proc)s configured for "
+                          "CpcSubset %(cpcsubset_name)s is greater than the "
+                          "available amount of processors %(max_proc)s on "
+                          "CPC object-id %(cpcid)s and CPC name %(cpcname)s")
+                        % {'config_proc': conf['max_processors'],
+                           'cpcsubset_name': conf['cpcsubset_name'],
+                           'max_proc': cpc.properties['processor-count-ifl'],
+                           'cpcid': conf['cpc_object_id'],
+                           'cpcname': cpc.properties['name']})
+            raise exception.ValidationError(errormsg)
+        if (conf['max_memory_mb'] > cpc.get_property('storage-customer')):
+            # TODO(preethipy): Exception infrastructure to be finalized
+            errormsg = (_("max_memory_mb %(config_mem)s configured for "
+                          "CpcSubset %(cpcsubset_name)s is greater than the "
+                          "available amount of memory %(max_mem)s on CPC "
+                          "object-id %(cpcid)s and CPC name %(cpcname)s")
+                        % {'config_mem': conf['max_processors'],
+                           'cpcsubset_name': conf['cpcsubset_name'],
+                           'max_mem': cpc.properties['processor-count-ifl'],
+                           'cpcid': conf['cpc_object_id'],
+                           'cpcname': cpc.properties['name']})
+            raise exception.ValidationError(errormsg)
     else:
         # TODO(preethipy): Exception infrastructure to be finalized
         raise Exception("Host not in DPM mode")
-
-    if (conf['max_processors'] > cpc.properties['processor-count-ifl']):
-        # TODO(preethipy): Exception infrastructure to be finalized
-        errormsg = (_("max_processors %(config_proc)s configured for "
-                      "CpcSubset %(cpcsubset_name)s is greater than the "
-                      "available amount of processors %(max_proc)s on "
-                      "CPC object-id %(cpcid)s and CPC name %(cpcname)s")
-                    % {'config_proc': conf['max_processors'],
-                       'cpcsubset_name': conf['cpcsubset_name'],
-                       'max_proc': cpc.properties['processor-count-ifl'],
-                       'cpcid': conf['cpc_object_id'],
-                       'cpcname': cpc.properties['name']})
-        raise exception.ValidationError(errormsg)
-    if (conf['max_memory_mb'] > cpc.properties['storage-customer']):
-        # TODO(preethipy): Exception infrastructure to be finalized
-        errormsg = (_("max_memory_mb %(config_mem)s configured for "
-                      "CpcSubset %(cpcsubset_name)s is greater than the "
-                      "available amount of memory %(max_mem)s on CPC "
-                      "object-id %(cpcid)s and CPC name %(cpcname)s")
-                    % {'config_mem': conf['max_processors'],
-                       'cpcsubset_name': conf['cpcsubset_name'],
-                       'max_mem': cpc.properties['processor-count-ifl'],
-                       'cpcid': conf['cpc_object_id'],
-                       'cpcname': cpc.properties['name']})
-        raise exception.ValidationError(errormsg)
 
 
 class PartitionState(object):
