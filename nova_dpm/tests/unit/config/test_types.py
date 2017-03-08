1dbf092210de9beee6f03df6c4ee90e81673a310
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2017-03-08 16:45:49 +0100

    Introduce storage adapter mapping conf object
    This patch introduces a oslo config object that can be used by the
    physical_storage_adapter_mappings configuration option.
    
    The config object validates the config option along a regex and
    returns a tuple (adapter_id, port).
    
    It accepts adapter-ids in upper and lower case. But for users
    it returns the adapter-id always in lower case (like desired
    by the HMC web services API).
    
    Some parts of this code can be removed once they are available
    in a new os-dpm release (e.g. the MultiStringWithKwargsType).
    
    Change-Id: I03319412ad15c608ecc43b0421f0cae42bd6b5dc
    Partial-Bug: #1663369

---
+++ nova_dpm/tests/unit/config/test_types.py
@@ -0,0 +1,99 @@
+# Copyright 2017 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nova.test import TestCase
+
+from nova_dpm.conf import types
+
+from nova_dpm.conf.types import MAPPING_REGEX
+from nova_dpm.conf.types import StorageAdapterMappingType
+
+VALID_DPM_OBJECT_ID = "fa1f2466-12df-311a-804c-4ed2cc1d656b"
+VALID_DPM_OBJECT_ID_UC = "FA1F2466-12DF-311A-804C-4ED2CC1D656B"
+VALID_STORAGE_MAPPING = VALID_DPM_OBJECT_ID + ":0"
+
+
+class TestStorageAdapterMappingType(TestCase):
+    conf_type = StorageAdapterMappingType()
+
+    def test_valid_mapping(self):
+        adapter_id, port = self.conf_type(VALID_STORAGE_MAPPING)
+        self.assertEqual("0", port)
+        self.assertEqual(VALID_DPM_OBJECT_ID, adapter_id)
+
+    def test_upper_case_to_lower_case_adapter_id(self):
+        adapter_id, port = self.conf_type(VALID_DPM_OBJECT_ID_UC + ":0")
+        self.assertEqual("0", port)
+        self.assertEqual(VALID_DPM_OBJECT_ID, adapter_id)
+
+    def test_empty_value_fail(self):
+        self.assertRaises(ValueError, self.conf_type, '')
+
+    def test_invalid_value_fail(self):
+        self.assertRaises(ValueError, self.conf_type, 'foobar')
+
+    def test_missing_port_fail(self):
+        self.assertRaises(ValueError, self.conf_type, VALID_DPM_OBJECT_ID)
+
+    def test_missing_port_fail_2(self):
+        self.assertRaises(ValueError, self.conf_type,
+                          VALID_DPM_OBJECT_ID + ":")
+
+    def test_invalid_port_fail(self):
+        self.assertRaises(ValueError, self.conf_type,
+                          VALID_DPM_OBJECT_ID + ":2")
+
+    def test_invalid_port_type_fail(self):
+        self.assertRaises(ValueError, self.conf_type,
+                          VALID_DPM_OBJECT_ID + ":a")
+
+    def test_invalid_adapter_id_fail(self):
+        self.assertRaises(ValueError, self.conf_type, "foo:1")
+
+    def test_repr(self):
+        self.assertEqual(
+            "String(regex='" + MAPPING_REGEX + "')",
+            repr(self.conf_type))
+
+    def test_equal(self):
+        self.assertTrue(
+            StorageAdapterMappingType() == StorageAdapterMappingType())
+
+
+# TODO(andreas_s): Remove once available in os-dpm
+class MultiStringWithKwargsType(TestCase):
+
+    def setUp(self):
+        super(MultiStringWithKwargsType, self).setUp()
+        self.mm_type = types.MultiStringWithKwargsType()
+
+    def test_format_default_none(self):
+        result = self.mm_type.format_defaults(None)
+        self.assertEqual([''], result)
+
+    def test_format_defaults_default_only(self):
+        result = self.mm_type.format_defaults(['foo', 'bar'])
+        self.assertEqual(['foo', 'bar'], result)
+
+    def test_format_defaults_sample_default_over_default(self):
+        result = self.mm_type.format_defaults(['foo', 'bar'], ['sample'])
+        self.assertEqual(['sample'], result)
+
+    def test_quote_trailing_and_leading_space_default(self):
+        result = self.mm_type.format_defaults([' foo ', ' bar '])
+        self.assertEqual(['" foo "', '" bar "'], result)
+
+    def test_quote_trailing_and_leading_space_sample_default(self):
+        result = self.mm_type.format_defaults([' foo ', ' bar '], [' sample '])
+        self.assertEqual(['" sample "'], result)
