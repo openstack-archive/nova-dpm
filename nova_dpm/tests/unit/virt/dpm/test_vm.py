b409342626087a8b5905c7cbe9c5431ad84e23e2
sreeteja <msreetej@in.ibm.com>
2017-04-12 15:34:23 +0530

    Added Unit Testcase for PartitionInstanceInfo class
    Change-Id: I1e9ea28e9d7b3caf987fe56c6272e3a6bb20a3b8

--- nova_dpm/tests/unit/virt/dpm/test_vm.py
+++ nova_dpm/tests/unit/virt/dpm/test_vm.py
@@ -12,22 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import requests.packages.urllib3
-import zhmcclient
-import time
-from nova.objects import fields
-from nova.compute import manager as compute_manager
+
 from nova.compute import power_state
 from nova.objects import instance as instance_obj
 from nova.test import TestCase
 from nova_dpm.tests.unit.virt.dpm import test_utils as utils
 from nova_dpm.virt.dpm import vm
-
+import requests.packages.urllib3
+import zhmcclient
 
 
 class PartitionInstanceInfoTestCase(TestCase):
-
-
 
     def setUp(self):
         super(PartitionInstanceInfoTestCase, self).setUp()
@@ -40,45 +35,37 @@ class PartitionInstanceInfoTestCase(TestCase):
         self.instance.uuid = '6511ee0f-0d64-4392-b9e0-aaaaaaaaaaaa'
         self.part_name = "OpenStack-foo-" + self.instance.uuid
         self.part_description = 'OpenStack CPCSubset=foo'
-        self.state= 'aCTIVE'
+        self.state = 'aCTIVE'
         self.create_partition_properties = {
             'name': self.part_name,
-            'description': self.part_description,'initial-memory': 1,'status':self.state,
-            'maximum-memory': 512,'ifl-processors':2
+            'description': self.part_description,
+            'initial-memory': 1,
+            'status': self.state,
+            'maximum-memory': 512,
+            'ifl-processors': 2
         }
 
         # Create partition in a cpc not from openstack
         # and used same uuid of instance to create
-        # vm.PartitionInstanceInfo class instance to get the information of the status,memory etc..,
+        # vm.PartitionInstanceInfo class instance to get the information
+        # of the status,memory etc..,
 
-        partition = self.cpc.partitions.create(
+        self.partition = self.cpc.partitions.create(
             self.create_partition_properties)
-
-
-        self.instance_partition=vm.PartitionInstanceInfo(self.instance,self.cpc)
-
+        self.instance_partition = vm.PartitionInstanceInfo(
+            self.instance, self.cpc)
 
     def test_state(self):
-
-        self.assertEqual(power_state.RUNNING,self.instance_partition.state)
+        self.assertEqual(power_state.RUNNING, self.instance_partition.state)
 
     def test_mem(self):
-
         self.assertEqual(1, self.instance_partition.mem)
 
-
     def test_max_mem(self):
-
         self.assertEqual(512, self.instance_partition.max_mem)
 
-
     def test_num_cpu(self):
-
         self.assertEqual(2, self.instance_partition.num_cpu)
 
-
     def test_cpu_time(self):
-
-        #Hardcoded-value for the cpu-time in vm.py
-         pass
-
+        pass
