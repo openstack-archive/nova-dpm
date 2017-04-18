173082c59cadcd5fdd80e63ba0bd9fe9b2499a25
Juergen Leopold <leopoldj@de.ibm.com>
2017-04-18 10:04:53 +0000

    Handles exception in init_host() if CPC is not found
    This code writes an error log in the nova log
    if the CPC is not found in the init_host() method
    of the DPMDriver. This will help to debug problems.
    The error message contains information about the
    HMC host, HMC userid and CPC UUID that were used.
    
    Closes-Bug: #1659037
    
    Change-Id: I4606cefc9943c0a1a3a0db0c496dcce5c468407f
    Signed-off-by: Juergen Leopold <leopoldj@de.ibm.com>

--- nova_dpm/virt/dpm/driver.py
+++ nova_dpm/virt/dpm/driver.py
@@ -33,6 +33,7 @@ from nova_dpm.virt.dpm import utils
 from nova_dpm.virt.dpm import vm
 from oslo_log import log as logging
 from oslo_utils import importutils
+from zhmcclient._exceptions import NotFound
 
 LOG = logging.getLogger(__name__)
 CONF = nova_dpm.conf.CONF
@@ -71,9 +72,16 @@ class DPMDriver(driver.ComputeDriver):
     def init_host(self, host):
         """Driver initialization of the hypervisor node"""
         LOG.debug("init_host")
+        try:
+            self._cpc = self._client.cpcs.find(**{
+                "object-id": CONF.dpm.cpc_object_id})
+        except NotFound:
+            LOG.error("Matching hypervisor %s not found for object-id %s "
+                      "and username %s on HMC %s",
+                      CONF.host, CONF.dpm.cpc_object_id,
+                      CONF.dpm.hmc_username, CONF.dpm.hmc)
+            return None
 
-        self._cpc = self._client.cpcs.find(**{
-            "object-id": CONF.dpm.cpc_object_id})
         LOG.debug("Matching hypervisor found %s for object-id %s and CPC %s",
                   CONF.host, CONF.dpm.cpc_object_id,
                   self._cpc.properties['name'])
