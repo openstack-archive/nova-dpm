9671cf8f8becfc8ad1fc6392ef73f583be37f2e3
sreeteja <msreetej@in.ibm.com>
2017-04-26 17:04:08 +0530

    Made config parameters manadatory
    closes-Bug: 1683318
    
    Change-Id: I80f8b36fa21cef313e0b5a821a26877afd1af980

--- nova_dpm/conf/dpm.py
+++ nova_dpm/conf/dpm.py
@@ -28,13 +28,13 @@ value `dpm.DPMDriver`.
 """
 
 ALL_DPM_OPTS = [
-    cfg.IntOpt('max_processors', help="""
+    cfg.IntOpt('max_processors', default=-1, help="""
     Maximum number of shared physical IFL processors on the target CPC that can
     be used for this OpenStack hypervisor host"""),
-    cfg.IntOpt('max_memory', help="""
+    cfg.IntOpt('max_memory', default=-1, help="""
     Maximum amount of memory (in MiB) on the target CPC that can be used for
     this OpenStack hypervisor host"""),
-    cfg.IntOpt('max_instances', help="""
+    cfg.IntOpt('max_instances', default=-1, help="""
     Maximum number of instances (partitions) that can be created for this
     OpenStack hypervisor host"""),
     MultiStorageAdapterMappingOpt('physical_storage_adapter_mappings', help="""
