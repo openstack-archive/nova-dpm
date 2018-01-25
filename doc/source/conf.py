3160070da5667c7295cfd5fcf47bf60af365a421
Mike Perez <thingee@gmail.com>
2018-01-25 12:16:36 +1100

    Replace support matrix ext with common library
    The code to generate a support matrix has been pulled into a common
    library. Using this instead of duplicating code in various projects that
    need it.
    
    Depends-On: I3ecea5f6a8b8e6bcfbc34019a2d2652ada8c9c57
    Change-Id: I8e3c14f22280d793c795dc5bccb383bce66606e0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('./'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'ext.support_matrix',
+    'sphinx_feature_classification.support_matrix',
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
     'sphinxcontrib.seqdiag',
